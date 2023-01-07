e3e36cd0dfafba573affef7aad5b38c47099f084
BrotherGrilka <exweerdo@gmail.com>
2023-01-07 03:54:03 -0500

    Ozzi's leaves

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,10 +2,14 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        "It works Mistr Ozz!"
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    
+    app.get("giggi") { req async -> String in
+        "Hello, Giggi!"
+    }
+    
+    app.get("ozzi") { req async throws -> View in
+        return try await req.view.render("ozzi", ["hiToOzzi": "Hi Ozz!"])
     }
 }
