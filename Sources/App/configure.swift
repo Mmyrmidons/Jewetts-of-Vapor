3b4301f73738b9b27894fc241d3c1715ad6cb623
BrotherGrilka <exweerdo@gmail.com>
2023-01-11 13:19:48 -0500

    Nuthin'

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,7 +3,6 @@ import Leaf
 
 public func configure(_ app: Application) throws {
     app.http.server.configuration.port = 3099
-
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.views.use(.leaf)
 
