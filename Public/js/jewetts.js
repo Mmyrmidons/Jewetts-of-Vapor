1c573cd022a55405e8eeafa0d3377c4f666e0d6d
BrotherGrilka <exweerdo@gmail.com>
2023-01-10 20:37:15 -0500

    Added starter css, js, icon and images

---
+++ Public/js/jewetts.js
@@ -0,0 +1,16 @@
+var jewetts = (function() {
+    var onReady = function() {
+		initEvents()
+	}
+	
+	var initEvents = function() {
+	}
+	
+    return {
+		onReady: onReady
+    }
+})()
+
+$(document).ready(function() {
+	jewetts.onReady()
+})
