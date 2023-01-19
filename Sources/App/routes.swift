1dd1ca48a30653d335f969f26b27f19bff6aaed7
BrotherGrilka <exweerdo@gmail.com>
2023-01-19 10:42:26 -0500

    Updated routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,15 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works Mistr Ozz!"
-    }
-    
-    app.get("giggi") { req async -> String in
-        "Hello, Giggi!"
+    app.get { req async throws -> View in
+        return try await req.view.render("ozzi", ["hiToOzzi": "Hi Ozz!"])
     }
     
     app.get("ozzi") { req async throws -> View in
         return try await req.view.render("ozzi", ["hiToOzzi": "Hi Ozz!"])
+    }
+
+    app.get("giggi") { req async -> String in
+        "Hello, Giggi!"
     }
 }
