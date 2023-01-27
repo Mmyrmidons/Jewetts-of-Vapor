a4aaeb5a313251f151dfa20be33a65391f1dd60d
BrotherGrilka <exweerdo@gmail.com>
2023-01-27 07:32:35 -0500

    Add links

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,13 @@
 import Vapor
 import Leaf
 
+struct Doctl {
+    let login = "doctl registry login"
+    let build = "docker build --platform=linux/amd64 -t jewetts:anni ."
+    let tag = "docker tag jewetts:anni registry.digitalocean.com/jewetts/jewetts:anni"
+    let push = "docker push registry.digitalocean.com/jewetts/jewetts:anni"
+}
+
 public func configure(_ app: Application) throws {
     app.http.server.configuration.port = 3099
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
