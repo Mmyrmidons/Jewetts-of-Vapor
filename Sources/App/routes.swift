3ecf34955e62ffb9ce31dbd4ad2d946884f27fb2
BrotherGrilka <exweerdo@gmail.com>
2023-01-30 16:54:09 -0500

    Sample XML transform

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -20,4 +20,25 @@ func routes(_ app: Application) throws {
     app.get("spudandal") { req async throws -> View in
         return try await req.view.render(pages.spudandal)
     }
+	
+//	hiGiggi()
+}
+
+func hiGiggi() {
+	if let cUrl = URL(string: "http://urtheaters.com/things/c.4.07.3.xml") {
+		do {
+			let cXml = try XMLDocument(contentsOf: cUrl, options: .nodePreserveAll)
+			
+			if let tUrl = URL(string: "http://urtheaters.com/things/title.xsl") {
+				do {
+					if let cData = try cXml.objectByApplyingXSLT(at: tUrl, arguments: nil) as? Data {
+						let cHtml = String(decoding: cData, as: UTF8.self)
+						print("Hi Giggi: ", cUrl, " :::: ", cHtml)
+						
+
+					}
+				} catch {}
+			}
+		} catch {}
+	}
 }
