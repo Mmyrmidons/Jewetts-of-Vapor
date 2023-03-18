6197d5c123c33eaafd43f6fb181b83d1087deace
BrotherGrilka <exweerdo@gmail.com>
2023-03-18 09:10:53 -0400

    Fraz-urb'luu

--- Sources/App/Controllers/pages.swift
+++ Sources/App/Controllers/pages.swift
@@ -4,7 +4,7 @@ struct Index: Codable {
 	init(_ app: Application) throws {
 		let imgLocation = "/img/various/"
 		let fileURL = URL(fileURLWithPath: app.directory.publicDirectory).appendingPathComponent(imgLocation, isDirectory: true)
-				
+
 		do {
 			let imgPaths = try FileManager.default.contentsOfDirectory(at: fileURL, includingPropertiesForKeys: nil).map {
 				imgLocation + $0.lastPathComponent
@@ -12,6 +12,27 @@ struct Index: Codable {
 			
 			pix = Array(imgPaths.prefix(4))
 		} catch { throw CheeseError.ImagePathError(error) }
+		
+		feed = Frazurbluu.feed
 	}
 	var pix:Array<String>
+	var feed:[Feed]
+}
+
+enum Reaction: Codable {
+	case like
+	case love
+	case haha
+	case wow
+	case sad
+	case angry
+	case nofuckingshit
+}
+
+struct Feed: Codable {
+	var message:String
+	var hashtags:[String]
+	var imageUrl:String
+	var reactions:[Reaction]
+ 	var shares:Int
 }
